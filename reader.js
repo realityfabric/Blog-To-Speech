6ed569fb4697868b266ffe858886650659c493f2
realityfabric <gaymoongoddess@gmail.com>
2015-09-17 06:19:15 -0700

    updated the code so it has some functionality

--- reader.js
+++ reader.js
@@ -1,9 +1,33 @@
 var post = $(".post_container");
-var holder = undefined;
-while (holder != "regular") {
-    post = post.nextSibling;
-    if (post.children != undefined && post.children[0].dataset != undefined) {
-         holder = post.children[0].dataset.type;
-    }
-}
-var post_content = $("#" + post.children[0].id + " > .post_wrapper > .post_content > .post_content_inner > .post_container > .post_body").textContent;
+var post_content; 
+
+var speak = function (msg) { 
+	var speech = new SpeechSynthesisUtterance(msg); 
+	speechSynthesis.speak(speech); 
+} 
+
+var cancel = function () { 
+	speechSynthesis.cancel(); 
+} 
+
+var next = function () { 
+	var holder = undefined; 
+	
+	while (holder != "regular") { 
+		post = post.nextSibling; 
+		
+		if (post.children != undefined && post.children[0].dataset != undefined) { 
+			holder = post.children[0].dataset.type; 
+		} 
+	} 
+	
+	post_content = $("#" + post.children[0].id + " > .post_wrapper > .post_content > .post_content_inner > .post_container > .post_body").textContent; 
+	
+	speak(post_content); 
+} 
+
+var init = function () { 
+	next(); 
+} 
+
+init();
