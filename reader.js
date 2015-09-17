b7ae82472631f254ac9d9a802f25a1efe50451e9
realityfabric <gaymoongoddess@gmail.com>
2015-09-17 06:36:47 -0700

    made a graceful exit in case of the program losing track of the posts (if the current post is null or undefined)

--- reader.js
+++ reader.js
@@ -11,12 +11,13 @@ var cancel = function () {
 } 
 
 var next = function () { 
+	if (post === undefined || post === null) { speak("Error: Post Doesn't Exist"); return; } //it's not going to work
 	var holder = undefined; 
 	
 	while (holder != "regular") { 
 		post = post.nextSibling; 
 		
-		if (post.children != undefined && post.children[0].dataset != undefined) { 
+		if (post.children != undefined && post.children.length > 0 && post.children[0].dataset != undefined) { 
 			holder = post.children[0].dataset.type; 
 		} 
 	} 
